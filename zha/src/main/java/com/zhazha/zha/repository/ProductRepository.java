7b2c52887c5b7752f88ba273bb2fbfd5ca9432aa
selizalam <147175598+selizalam@users.noreply.github.com>
2023-11-13 16:39:15 -0500

    Update ProductRepository.java

--- zha/src/main/java/com/zhazha/zha/repository/ProductRepository.java
+++ zha/src/main/java/com/zhazha/zha/repository/ProductRepository.java
@@ -1,5 +1,15 @@
 package com.zhazha.zha.repository;
 
-public class ProductRepository {
-    
+import com.zhazha.zha.model.Product;
+
+import reactor.core.publisher.Flux;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends R2dbcRepository<Customer, Integer>{
+    Flux<Product> findByProductID(int id);
+
+    Flux<Product> findByProductName(String name);
 }
